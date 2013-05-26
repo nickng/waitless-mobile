bb7d67f6023c3de90b84647db98904a6bc44c3fc
Nicholas Ng <nickng@projectfate.org>
2013-05-26 15:06:16 +0100

    add new JS file

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var loc=document.getElementById("loc");
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML="Geolocation is not supported by this browser.";
+  }
+
+}
+function showPosition(position) {
+  loc.value = position.coords.longitude+","+position.coords.latitude;
+}
+
+getLocation();
